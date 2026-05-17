5462fceb9eb7a91d7ac5ff0b45a078b1adabc65e
SameDesu123 <47582414+SameDesu123@users.noreply.github.com>
2026-05-17 11:15:12 +0900

    Loosen raw storage value typing

--- src/ts/storage/storageTypes.ts
+++ src/ts/storage/storageTypes.ts
@@ -1,6 +1,6 @@
 export type RisuStorageKey = string
 
-export type RisuStorageReadableValue = Buffer | null
+export type RisuStorageReadableValue = unknown
 
 export type RisuStorageWritableValue = Uint8Array
 
