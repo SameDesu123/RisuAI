8f420ef1472dc08ba6313293043b853be5312364
SameDesu123 <47582414+SameDesu123@users.noreply.github.com>
2026-05-17 15:54:51 +0900

    Merge workspace storage translations

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -6,30 +6,36 @@ import { languageKorean } from "./ko";
 import { languageVietnamese } from "./vi";
 import { languageChineseTraditional } from "./zh-Hant";
 import { languageSpanish } from "./es";
+import { workspaceStorageTranslations, type WorkspaceStorageLanguage } from "./workspaceStorage";
 
-export let language:typeof languageEnglish = languageEnglish
+export let language:typeof languageEnglish = mergeWorkspaceStorage(languageEnglish, 'en')
 
+function mergeWorkspaceStorage<T extends typeof languageEnglish>(base: T, lang: WorkspaceStorageLanguage): T {
+    return merge(safeStructuredClone(base), {
+        workspaceStorage: workspaceStorageTranslations[lang]
+    })
+}
 
 export function changeLanguage(lang:string){
     if(lang === 'cn'){
-        language = merge(safeStructuredClone(languageEnglish), languageChinese)
+        language = merge(mergeWorkspaceStorage(languageEnglish, 'cn'), languageChinese)
     }
     else if(lang === 'de'){
-        language = merge(safeStructuredClone(languageEnglish), languageGerman)
+        language = merge(mergeWorkspaceStorage(languageEnglish, 'de'), languageGerman)
     }
     else if(lang === 'ko'){
-        language = merge(safeStructuredClone(languageEnglish), languageKorean)
+        language = merge(mergeWorkspaceStorage(languageEnglish, 'ko'), languageKorean)
     }
     else if(lang === 'vi'){
-        language = merge(safeStructuredClone(languageEnglish), languageVietnamese)
+        language = merge(mergeWorkspaceStorage(languageEnglish, 'vi'), languageVietnamese)
     }
     else if(lang === 'zh-Hant'){
-        language = merge(safeStructuredClone(languageEnglish), languageChineseTraditional)
+        language = merge(mergeWorkspaceStorage(languageEnglish, 'zh-Hant'), languageChineseTraditional)
     }
     else if(lang === 'es'){
-        language = merge(safeStructuredClone(languageEnglish), languageSpanish)
+        language = merge(mergeWorkspaceStorage(languageEnglish, 'es'), languageSpanish)
     }
     else{
-        language = languageEnglish
+        language = mergeWorkspaceStorage(languageEnglish, 'en')
     }
 }
