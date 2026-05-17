4c0fc9ac0175c4d9635d8ddfa2f17264e4dcd200
SameDesu123 <47582414+SameDesu123@users.noreply.github.com>
2026-05-17 11:14:49 +0900

    Add shared storage interface types

---
+++ src/ts/storage/storageTypes.ts
@@ -0,0 +1,15 @@
+export type RisuStorageKey = string
+
+export type RisuStorageReadableValue = Buffer | null
+
+export type RisuStorageWritableValue = Uint8Array
+
+export type RisuStorageKeyList = string[] | Promise<string[]>
+
+export interface RisuRawStorage {
+    setItem(key: RisuStorageKey, value: RisuStorageWritableValue): Promise<unknown>
+    getItem(key: RisuStorageKey): Promise<RisuStorageReadableValue>
+    keys(): RisuStorageKeyList
+    removeItem(key: RisuStorageKey): Promise<unknown> | unknown
+    listItem?: () => RisuStorageKeyList
+}
